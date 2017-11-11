86444d349837d6116ed3c60ee156166ef4062a2f
Basil LK <32955738+Basil-LK@users.noreply.github.com>
2017-11-11 23:20:57 +0100

    Fixing resolving conflicts
    status: Finished

--- SoftProj/src/basicClass/Address.java
+++ SoftProj/src/basicClass/Address.java
@@ -1,116 +1,3 @@
-<<<<<<< HEAD
-package basicClass;
-
-
-
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
-
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.cfg.Configuration;
-
-@Entity
-@Table(name="Address")
-public class Address {
-	
-	@Id
-	//@Column(name="addressId")
-	private int AddressId;
-	//@Column(name="streetname")
-	private String streetname;
-	//@Column(name="houseNumber)
-	private int houseNumber;
-	//@Column(name="city")
-	private String city;
-	//@Column(name="visibility")
-	private int visibility;
-	
-	public Address(String streetname, int houseNumber, String city) {
-		super();
-		this.streetname = streetname;
-		this.houseNumber = houseNumber;
-		this.city = city;
-	}
-	public Address(int AddressId,String streetname, int houseNumber, String city) {
-		super();
-		this.AddressId = AddressId;
-		this.streetname = streetname;
-		this.houseNumber = houseNumber;
-		this.city = city;
-	}
-	public Address() {
-		
-	}
-	public String getStreetname() {
-		return streetname;
-	}
-	public void setStreetname(String streetname) {
-		this.streetname = streetname;
-	}
-	public int getHouseNumber() {
-		return houseNumber;
-	}
-	public void setHouseNumber(int houseNumber) {
-		this.houseNumber = houseNumber;
-	}
-	public String getCity() {
-		return city;
-	}
-	public void setCity(String city) {
-		this.city = city;
-	}
-	public int getAddressId() {
-		return AddressId;
-	}
-	public void setAddressId(int AddressId) {
-		this.AddressId = AddressId;
-	}
-	public int getVisibility() {
-		return visibility;
-	}
-	public void setVisibility(int visibility) {
-		this.visibility = visibility;
-	}
-		
-		/////////////////////////////////////////////////////
-		///////////////////////DAO///////////////////////////
-		/////////////////////////////////////////////////////
-	
-public static void main(String[] args) {
-		
-	
-		
-		
-			
-			
-			
-			
-			//opening session
-			
-			SessionFactory sessionFactory = new Configuration().configure().addAnnotatedClass(Address.class).buildSessionFactory();
-			Session session = sessionFactory.openSession();
-			session.beginTransaction();
-			Address address = new Address();
-			
-			
-			//Statement UPDATE
-			address =(Address) session.get(Address.class, 0);
-			//address.setStreetname("updated");
-			//session.update(address);
-			
-			
-			//closing session
-			session.getTransaction().commit();
-			System.out.println("Statement Worked!");
-			session.close();
-			sessionFactory.close();
-
-		}
-}
-	
-=======
 package basicClass;
 
 import javax.persistence.Column;
@@ -198,7 +85,7 @@ address = (Address) session.get(Address.class, 1);
 
 //Update statement DAO ADDRESS
 
-address.setVisibility(1);
+address.setVisibility(0);
 session.update(address);
 
 
@@ -211,11 +98,11 @@ sessionFactory.close();
 
 }
 
-public static void main(String[] args) {
+	public static void main(String[] args) {
 	
 	new Address().addressDAO();
-}
+	}
 
 
 }
->>>>>>> 8e63f21d229b3025738230cb5752d84f72c639fc
+
